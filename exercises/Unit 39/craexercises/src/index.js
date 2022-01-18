1426921dbb8b94cf0dc149cee2dd6a29ed13f23d
Emma <emma.pines.s@gmail.com>
2022-01-17 16:40:45 -0800

    finished exercise

---
+++ exercises/Unit 39/craexercises/src/index.js
@@ -0,0 +1,9 @@
+import { choice, remove } from "./helpers";
+import fruits from "./foods";
+
+const RANDOMFRUIT = choice(fruits);
+console.log(`I'd like one ${RANDOMFRUIT}, please.`);
+console.log(`Here you go: ${RANDOMFRUIT}`);
+console.log(`Delicious! May I have another?`);
+const FRUITSLEFT = remove(fruits, RANDOMFRUIT);
+console.log(`I'm sorry, we're all out. We have ${FRUITSLEFT.length} left`)
